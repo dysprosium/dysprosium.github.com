70398a6cb530d8dff948992686e456aeb3a4d6b3
Christopher Liu <universelkl@gmail.com>
2013-03-08 14:16:43 +0800

    primeCheck.js
    Added prime checker to homepage. It's better than before, too! yay! Now
    2 displays as being prime.

---
+++ javascripts/primeCheck.js
@@ -0,0 +1,43 @@
+var primeCheck = function() {
+	var number;
+	var primeValue;
+	var divisor;
+	var skip = true;
+	while (skip !== false) {
+		number = (prompt("Find out if the number is prime:"));
+		var skip1 = false;
+		var skip2 = false;
+		var skip3 = false;
+		var skip4 = false;
+		var skip5 = false;
+		if (number == 1) {
+			alert ("The number is not prime");
+			skip1 = true;
+		} if (number == 2) {
+			alert ("The number is prime");
+			skip2 = true;
+		} if (isNaN(number) === true) {
+			alert ("Please enter a valid number");
+			skip3 = true;
+		} if (number < 1) {
+			alert ("Please enter a valid number");
+			skip4 = true;
+		} if (number % 1 !== 0 & skip2 !== true) {
+			alert ("Please enter a valid number");
+			skip5 = true;
+		} if (skip1 === false & skip2 === false & skip3 === false & skip4 === false) {
+			skip = false;
+		}
+	}
+	for (divisor = 2; divisor < Math.sqrt(number) + 1; divisor++) {
+		if (number % divisor === 0) {
+			primeValue = 1;
+			break;
+		}
+	}
+	if (primeValue == 1) {
+		alert ("The number is not prime");
+	} else {
+		alert ("The number is prime");
+	}
+}; // max number = 8999999999999999
--- javascripts/totallynotahack.js
+++ javascripts/totallynotahack.js
@@ -2,15 +2,15 @@ function totallynotahack() {
 	var bin;
 	function newbin() {
 		var x = Math.floor(Math.random() * 90000) + 10000;
-		x = parseInt(x);
+		x = parseInt(x, 10);
 		var bin = x.toString(2);
 		return bin;
 	}
 	newbin();
-	for (i = 1; i < 51; i++) {
-		for (j = 1; j < 11; j++) {
+	for (i = 1; i < 1001; i++) {
+		for (j = 1; j < 12; j++) {
 			document.write(newbin());
 		}
-		document.write("\n")
+		document.write("\n");
 	}
 }
